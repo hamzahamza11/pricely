a171d41ff3ed7464e75fc31a3313b694ce470946
hamza <sabirha00@gmail.com>
2021-06-01 13:31:08 +0100

    add the logo and picture

--- src/components/Header-Footer/header.js
+++ src/components/Header-Footer/header.js
@@ -2,8 +2,8 @@ function Header() {
     return (
       <div className="navbar">
         <div className="navbar_logo">
-          <div className="navbar_logo-icon"></div>
-          <h3>pricely</h3>
+          {/* <div className="navbar_logo-icon"></div>
+          <h3>pricely</h3> */}
         </div>
         <div className="navbar_links">
           <a href="#">Alerts</a>
